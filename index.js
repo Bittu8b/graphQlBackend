c6809a24439adc063915035085ddccd71ce5b28c
Saubhik Singh <saubhik.singh@gslab.com>
2019-04-19 11:29:00 +0530

    initial

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const graphqlHttp = require("express-graphql");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
 
-const graphQlSchema = require("./GraphQL/Schema/index.js");
-const graphQlResolver = require("./GraphQL/Resolver/index.js");
-const isAuth = require("./middleware/is-auth");
-const cors = require("cors");
+const graphQlSchema = require('./GraphQL/Schema/index.js');
+const graphQlResolver = require('./GraphQL/Resolver/index.js');
+const isAuth = require('./middleware/is-auth');
+const cors = require('cors');
 
 const app = express();
 
@@ -16,8 +16,12 @@ app.use(bodyParser.json());
 
 app.use(isAuth);
 
+app.get('/', (req, res) => {
+  res.json({ status: 'You have reached to home' });
+});
+
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHttp({
     schema: graphQlSchema,
     rootValue: graphQlResolver,
@@ -27,11 +31,11 @@ app.use(
 
 mongoose
   .connect(
-    "mongodb+srv://admin:007@graphql-cluster-v8me6.mongodb.net/graphql-dev?retryWrites=true",
+    'mongodb+srv://admin:007@graphql-cluster-v8me6.mongodb.net/graphql-dev?retryWrites=true',
     { useNewUrlParser: true }
   )
   .then(() => {
-    console.log("Connection Successful");
+    console.log('Connection Successful');
   })
   .catch(err => {
     console.log(err);
